e5b9957d3dfd42fe6b800c6441143e2cb47ef65b
MURlL0 <muriilorodrigues@outlook.com>
2023-07-12 01:55:02 -0300

    inicial incons menu

--- Site-Portifolio/js/script.js
+++ Site-Portifolio/js/script.js
@@ -1,5 +1,6 @@
 let dropdown = document.querySelector('.dropdown');
 let toggle = document.querySelector('.toggle');
+let toggleClose = document.querySelector('.toggleClose');
 
 let open = false;
 
@@ -7,9 +8,13 @@ function openClose() {
     if (open === true) {
         open = false;
         dropdown.classList.remove('open');
+        toggleClose.classList.remove('open');
+        toggle.classList.add('open');
     } else if (open === false) {
         open = true;
         dropdown.classList.add('open');
+        toggleClose.classList.add('open');
+        toggle.classList.remove('open');
     }
 }
 
@@ -17,11 +22,12 @@ toggle.onclick = openClose;
 
 function closeMenu() {
     dropdown.classList.remove('open');
+    toggleClose.classList.remove('open');
 }
 
 dropdown.addEventListener('click', function (event) {
     if (event.target.tagName === 'A') {
-        openClose();
+        closeMenu();
     }
 });
 
