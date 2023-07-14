166203f1992d8015267c14dda00ca1804bdc7fe0
MURlL0 <muriilorodrigues@outlook.com>
2023-07-13 23:59:42 -0300

    new btn menu

--- barber_shop/js/script.js
+++ barber_shop/js/script.js
@@ -17,6 +17,8 @@ document.querySelector('#servicos').innerHTML = options;
 document.querySelector('#servicos').innerHTML = options;
 
 let dropdown = document.querySelector('.dropdown');
+let toggle = document.querySelector('.toggle');
+let toggleClose = document.querySelector('.toggleClose');
 
 let open = false;
 
@@ -24,8 +26,25 @@ function openMenu() {
     if (open === true) {
         open = false;
         dropdown.classList.remove('open');
+        toggle.classList.remove('open');
+        toggleClose.classList.remove('open');
     } else if (open === false) {
         open = true;
         dropdown.classList.add('open');
+        toggle.classList.add('open');
+        toggleClose.classList.add('open');
     }
 }
+
+toggle.onclick= openMenu
+toggleClose.onclick= openMenu
+
+function closeMenu() {
+    dropdown.classList.remove('open');
+    toggle.classList.remove('open');
+    toggleClose.classList.remove('open');
+}
+
+dropdown.addEventListener('click', function (elemento) {
+    if (elemento.target.tagName === 'A') closeMenu();
+});
