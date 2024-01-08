d18abb6c110b9444d8c7672a4841af0f67bd3eb0
MURlL0 <muriilorodrigues@outlook.com>
2024-01-07 21:34:45 -0300

    update completo e incompleto

---
+++ burger_king/js/com_in.js
@@ -0,0 +1,40 @@
+const completo = () => {
+    const numberComIn = document.getElementById('comIn').value;
+    const numberVenda = document.getElementById('venda').value;
+
+    const comIn = parseInt(numberComIn);
+    const venda = parseInt(numberVenda);
+
+    const result2 = document.getElementById('result2');
+    const resultado = (comIn / venda) * 100;
+    const form = document.completo;
+
+    if (form.comIn.value == '') {
+        document.getElementById(
+            'result2'
+        ).innerHTML = `Digite o Completo ou Incompleto`;
+        result2.classList.remove('negative');
+    } else if (isNaN(comIn)) {
+        document.getElementById('result2').innerHTML = `Somente Número`;
+    } else if (form.venda.value == '') {
+        document.getElementById('result2').innerHTML = `Digite o valor venda`;
+        result2.classList.remove('negative');
+    } else if (isNaN(venda)) {
+        document.getElementById('result2').innerHTML = `Somente Número`;
+    } else {
+        document.getElementById('result2').innerHTML = `${resultado.toFixed(
+            2
+        )}%`;
+    }
+    if (resultado < 0) {
+        result2.classList.add('negative');
+    } else {
+        result2.classList.remove('negative');
+    }
+};
+
+const btnEnter = (event) => {
+    if (event.keyCode === 13) {
+        completo();
+    }
+};
---
+++ burger_king/js/venda.js
@@ -0,0 +1,36 @@
+const calculo = () => {
+    const numberReal = document.getElementById('real').value;
+    const numberEstimado = document.getElementById('estimado').value;
+
+    const real = parseInt(numberReal);
+    const estimado = parseInt(numberEstimado);
+
+    const result = document.getElementById('result');
+    const calc = ((real - estimado) / estimado) * 100;
+    const form = document.calculo;
+
+    if (form.real.value == '') {
+        document.getElementById('result').innerHTML = `Digite o valor Real`;
+        result.classList.remove('negative');
+    } else if (isNaN(real)) {
+        document.getElementById('result').innerHTML = `Somente Número`;
+    } else if (form.estimado.value == '') {
+        document.getElementById('result').innerHTML = `Digite o valor Estimado`;
+        result.classList.remove('negative');
+    } else if (isNaN(estimado)) {
+        document.getElementById('result').innerHTML = `Somente Número`;
+    } else {
+        document.getElementById('result').innerHTML = `${calc.toFixed(2)}%`;
+    }
+    if (calc < 0) {
+        result.classList.add('negative');
+    } else {
+        result.classList.remove('negative');
+    }
+};
+
+const btnEnter = (event) => {
+    if (event.keyCode === 13) {
+        calculo();
+    }
+};
