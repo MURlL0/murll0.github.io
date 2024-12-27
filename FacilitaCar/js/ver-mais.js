2f43462cee19efd066bb866a80796ac812a1ac64
Murilo Rodrigues <118283751+MURlL0@users.noreply.github.com>
2024-12-27 13:03:48 -0300

    removed car

--- FacilitaCar/js/ver-mais.js
+++ FacilitaCar/js/ver-mais.js
@@ -30,36 +30,7 @@ showSlide(currentSlide);
 
 // Dados de exemplo para os carros
 const carros = {
-  1: {
-    nome: 'Nissan Versa',
-    preco: 'R$ 44.816,00',
-    imagens: [
-      '../assets/img/Veiculos/Nissan/1.Versa/Versa 2018 Prata.jpg',
-      '../assets/img/Veiculos/Nissan/1.Versa/Nissan Versa SV 2018.jpg',
-      '../assets/img/Veiculos/Nissan/1.Versa/Nissan Versa SV 2018 (1).jpg',
-    ],
-    descricao: 'Elegância e eficiência, perfeito para quem busca conforto.',
-    marca: 'Nissan',
-    modelo: 'Versa',
-    ano: '2019',
-    cor: 'Prata',
-    transmissao: 'Automático',
-  },
-  2: {
-    nome: 'Volkswagen Voyage',
-    preco: 'R$ 40.317,00',
-    imagens: [
-      '../assets/img/Veiculos/Volkswagen/2.Voyage/Volkswagen Voyage 2015.jpg',
-      '../assets/img/Veiculos/Volkswagen/2.Voyage/Volkswagen Voyage 2015 (1).jpg',
-      '../assets/img/Veiculos/Volkswagen/2.Voyage/Volkswagen Voyage 2015 (2).jpg',
-    ],
-    descricao: 'Um sedã clássico com ótimo custo-benefício e espaço interno.',
-    marca: 'Volkswagen',
-    modelo: 'Voyage',
-    ano: '2015',
-    cor: 'Prata',
-    transmissao: 'Manual',
-  },
+
   3: {
     nome: 'Ford Ka Sedan',
     preco: 'R$ 47.460,00',
@@ -75,51 +46,7 @@ const carros = {
     cor: 'Cinza',
     transmissao: 'Manual',
   },
-  4: {
-    nome: 'Fiat Mobi',
-    preco: 'R$ 33.580,00',
-    imagens: [
-      '../assets/img/Veiculos/Fiat/4.Mobi/Fiat Mobi Drive 2018.jpg',
-      '../assets/img/Veiculos/Fiat/4.Mobi/Fiat Mobi 2018.jpg',
-      '../assets/img/Veiculos/Fiat/4.Mobi/Fiat Mobi 2018 (1).jpg',
-    ],
-    descricao: 'Ideal para trajetos urbanos, com ótima economia.',
-    marca: 'Fiat',
-    modelo: 'Mobi',
-    ano: '2018',
-    cor: 'Branco',
-    transmissao: 'Manual',
-  },
-  5: {
-    nome: 'Fiat Palio',
-    preco: 'R$ 28.149,00',
-    imagens: [
-      '../assets/img/Veiculos/Fiat/5.Palio/Fiat Palio 2015.jpg',
-      '../assets/img/Veiculos/Fiat/5.Palio/Fiat Palio 2015 (1).jpg',
-      '../assets/img/Veiculos/Fiat/5.Palio/Fiat Palio 1.0 2015.jpg',
-    ],
-    descricao: 'Compacto, econômico e confiável, ideal para o dia a dia.',
-    marca: 'Fiat',
-    modelo: 'Palio',
-    ano: '2015',
-    cor: 'Vermelho',
-    transmissao: 'Manual',
-  },
-  6: {
-    nome: 'Citroën C3',
-    preco: 'R$ 49.859,00',
-    imagens: [
-      '../assets/img/Veiculos/Citroen/6.C3/Citroen C3 2016.jpg',
-      '../assets/img/Veiculos/Citroen/6.C3/Citroen C3 2016 (1).jpg',
-      '../assets/img/Veiculos/Citroen/6.C3/Citroen C3 2016 (2).jpg',
-    ],
-    descricao: 'Design sofisticado, conforto e economia em um único veículo.',
-    marca: 'Citroën',
-    modelo: 'C3',
-    ano: '2016',
-    cor: 'Preto',
-    transmissao: 'Automático',
-  },
+
   8: {
     nome: 'Arrizo 5',
     preco: 'R$ 66.000,00',
@@ -144,116 +71,6 @@ const carros = {
     transmissao: 'Automático',
   },
 
-  10: {
-    nome: 'Chevrolet Cobalt',
-    preco: 'R$ 37.553,00',
-    imagens: [
-      '../assets/img/Veiculos/Chevrolet/10.Cobalt/Chevrolet Cobalt LTZ 2014.jpg',
-      '../assets/img/Veiculos/Chevrolet/10.Cobalt/Chevrolet Cobalt LTZ 2014 (1).jpg',
-      '../assets/img/Veiculos/Chevrolet/10.Cobalt/Chevrolet Cobalt LTZ 2014 (2).jpg',
-    ],
-    descricao: 'Espaçoso, confortável e com excelente desempenho.',
-    marca: 'Chevrolet',
-    modelo: 'Cobalt',
-    ano: '2014',
-    cor: 'Preta',
-    transmissao: 'Manual',
-  },
-
-  11: {
-    nome: 'Peugeot 208',
-    preco: 'R$ 37.031,00',
-    imagens: [
-      '../assets/img/Veiculos/Peugeot/11.208/999413100136142.jpg',
-      '../assets/img/Veiculos/Peugeot/11.208/Peugeot 208 Allure 2014.jpg',
-      '../assets/img/Veiculos/Peugeot/11.208/Peugeot 208 Allure 2014 (1).jpg',
-    ],
-    descricao: 'Compacto, ágil e com um design moderno, ideal para a cidade.',
-    marca: 'Peugeot',
-    modelo: '208',
-    ano: '2014',
-    cor: 'Vermelha',
-    transmissao: 'Manual',
-  },
-  12: {
-    nome: 'Renault Logan',
-    preco: 'R$ 37.036,00',
-    imagens: [
-      '../assets/img/Veiculos/Renault/12.Logan/Renault Logan 2015.jpg',
-      '../assets/img/Veiculos/Renault/12.Logan/Renault Logan 2015 (1).jpg',
-      '../assets/img/Veiculos/Renault/12.Logan/Renault Logan 2015 (2).jpg',
-    ],
-    descricao: 'Espaçoso, confiável e com excelente economia de combustível.',
-    marca: 'Renault',
-    modelo: 'Logan',
-    ano: '2015',
-    cor: 'Cinza',
-    transmissao: 'Manual',
-  },
-  13: {
-    nome: 'Renault Logan',
-    preco: 'R$ 37.357,00',
-    imagens: [
-      '../assets/img/Veiculos/Renault/13.Logan/Renault Logan 2016.jpg',
-      '../assets/img/Veiculos/Renault/13.Logan/Renault Logan 2016 (1).jpg',
-      '../assets/img/Veiculos/Renault/13.Logan/Renault Logan 2016 (2).jpg',
-    ],
-    descricao:
-      'Design moderno, espaço interno generoso e excelente custo-benefício.',
-    marca: 'Renault',
-    modelo: 'Logan',
-    ano: '2016',
-    cor: 'Prata',
-    transmissao: 'Manual',
-  },
-  14: {
-    nome: 'Ford Ka Sedan',
-    preco: 'R$ 48.558,00',
-    imagens: [
-      '../assets/img/Veiculos/Ford/14.Ka Sedan/Ford Ka 1.0 SE 2019.jpg',
-      '../assets/img/Veiculos/Ford/14.Ka Sedan/Ford Ka 1.0 SE 2019 (1).jpg',
-      '../assets/img/Veiculos/Ford/14.Ka Sedan/Ford Ka 1.0 SE 2019 (2).jpg',
-    ],
-    descricao: 'Compacto, espaçoso e eficiente para o dia a dia.',
-    marca: 'Ford',
-    modelo: 'Ka Sedan',
-    ano: '2019',
-    cor: 'Cinza',
-    transmissao: 'Manual',
-  },
-  15: {
-    nome: 'Ford Fusion Titanium',
-    preco: 'R$ 86.062,00',
-    imagens: [
-      '../assets/img/Veiculos/Ford/15.Fusion/Ford Fusion Titanium 2016.jpg',
-      '../assets/img/Veiculos/Ford/15.Fusion/Ford Fusion Titanium 2016 (1).jpg',
-      '../assets/img/Veiculos/Ford/15.Fusion/Ford Fusion Titanium 2016 (2).jpg',
-    ],
-    descricao: 'Luxuoso, confortável e com alta performance.',
-    marca: 'Ford',
-    modelo: 'Fusion Titanium',
-    ano: '2016',
-    cor: 'Branca',
-    transmissao: 'Automático',
-  },
-  16: {
-    nome: 'Jeep Renegade',
-    preco: 'R$ 82.000,00',
-    imagens: [
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.13 2.jpeg',
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.12 (2).jpeg',
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.12.jpeg',
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.11.jpeg',
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.13 (1).jpeg',
-      '../assets/img/Veiculos/Jeep/16.Renegade/WhatsApp Image 2024-11-28 at 17.16.12 (1).jpeg',
-    ],
-    descricao: 'Estilo robusto, alta performance e ideal para aventuras.',
-    marca: 'Jeep',
-    modelo: 'Renegade',
-    ano: '2021',
-    cor: 'Preta',
-    transmissao: 'Automático',
-  },
   17: {
     nome: 'Volkswagen Polo',
     preco: 'R$ 84.000,00',
@@ -279,21 +96,7 @@ const carros = {
     cor: 'Cinza',
     transmissao: 'Automático',
   },
-  18: {
-    nome: 'Nissan Versa',
-    preco: 'R$ 48.000,00',
-    imagens: [
-      '../assets/img/Veiculos/Nissan/18.Versa/WhatsApp Image 2024-12-05 at 22.11.49.jpeg',
-      '../assets/img/Veiculos/Nissan/18.Versa/WhatsApp Image 2024-12-05 at 22.11.48.jpeg',
-      '../assets/img/Veiculos/Nissan/18.Versa/WhatsApp Image 2024-12-05 at 22.11.49 2.jpeg',
-    ],
-    descricao: 'Elegância com tecnologia moderna e conforto incomparável.',
-    marca: 'Nissan',
-    modelo: 'Versa',
-    ano: '2016',
-    cor: 'Preta',
-    transmissao: 'Manual',
-  },
+
   19: {
     nome: 'Fiat Uno',
     preco: 'R$ 48.000,00',
@@ -309,6 +112,7 @@ const carros = {
     cor: 'Prata',
     transmissao: 'Manual',
   },
+
 };
 
 // Função para extrair o ID do carro da URL
